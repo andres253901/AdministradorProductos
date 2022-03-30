9334bf418d319039d1d14e07540bd546823610fa
diego.pirazan <diegoa253901@hotmail.com>
2022-03-29 19:39:10 -0500

    Prueba unitaria

--- test/co/endava/app/AdministradorProductosTest.java
+++ test/co/endava/app/AdministradorProductosTest.java
@@ -1,7 +1,37 @@
 package co.endava.app;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdministradorProductosTest {
+    @Test
+    void main() {
 
-}
+        String nombre="Agua";
+        Double descuentoTest= 900.0;
+        Double precioTest = 1800.0;
+        Producto p5 = new Producto(105, "Agua", 1800);
+
+        //Validacion Nombre
+        assertEquals(nombre,p5.getNombre());
+
+        //Validacion Descuento
+        assertEquals(descuentoTest,p5.getDescuento());
+    }
+
+
+    public double getDescuento(Double precio){
+        return precio * 0.50;
+    }
+        }
+
+
+        // public void descuento() {
+
+       // double porcentajeControl = 10;
+       //Comida p3 = new Comida(103, "Pastel", 2000, LocalDate.now().plusDays(2));
+       // Double descuento = p3.getDescuento();
+       //Double percentProduct = descuento * 100 / p3.getPrecio();
+
+       // assertEquals(porcentajeControl, percentProduct);
+        }//
